daed34cbd678cc34fc3e45c10011528dc989083c
laineus <lainen000@gmail.com>
2021-01-17 00:48:33 +0900

    promise

--- src/extrude.js
+++ src/extrude.js
@@ -1,17 +1,15 @@
 #!/usr/bin/env node
 
 const path = require('path')
-const fs = require('fs')
+const fs = require('fs').promises
 const { extrudeTilesetToBuffer } = require('tile-extruder')
 const imagemin = require('imagemin')
 const imageminPngquant = require('imagemin-pngquant')
 
 const pngquant = imageminPngquant()
 
-module.exports = (input, outputDir, size, minify) => {
-  return extrudeTilesetToBuffer(size, size, input).then(buffer => {
-    return imagemin.buffer(buffer, minify ? { plugins: [pngquant] } : {}).then(minifiedBuffer => {
-      return fs.writeFileSync(path.join(outputDir, path.basename(input)), minifiedBuffer)
-    })
-  })
+module.exports = async (input, outputDir, size, minify) => {
+  const buffer = await extrudeTilesetToBuffer(size, size, input)
+  const minifiedBuffer = await imagemin.buffer(buffer, minify ? { plugins: [pngquant] } : {})
+  await fs.writeFile(path.join(outputDir, path.basename(input)), minifiedBuffer)
 }
